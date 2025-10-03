7bfbd194380478e4fa4e28de065e5b4f12c59b35
Óscar <oscar.serrano91@gmail.com>
2025-10-03 12:24:28 +0200

    for loops

--- HelloApp/02 - Logic/Loops.cs
+++ HelloApp/02 - Logic/Loops.cs
@@ -8,7 +8,7 @@ partial class Program
 
         while (counter <= 5)
         {
-            WriteLine($"Iteraçao {counter}");
+            //WriteLine($"Iteraçao {counter}");
             counter++;
         }
 
@@ -18,8 +18,39 @@ partial class Program
 
         do
         {
-            WriteLine($"Número: {number}");
-            number++; 
-        } while (number < 3); 
+            //WriteLine($"Número: {number}");
+            number++;
+        } while (number < 3);
+
+
+        //bucles for
+
+        for (int i = 10; i >= 0; i -= 2)
+        {
+
+            if (i == 4)
+            {
+                WriteLine("No queremos imprimir el 4");
+                continue; 
+            }
+            
+            WriteLine($"Iteración: {i}");
+        }
+
+        //Array foreach;
+        string[] fruits = ["Manzana", "Piña", "Melocotón", "Sandía"];
+
+        foreach (var fruit in fruits)
+        {
+            //WriteLine(fruit);
+        }
+
+        //List foreach
+        List<string> names = ["Pedro", "Paco", "Susana"];
+
+        foreach (var name in names)
+        {
+            //WriteLine(name); 
+        }
     }
 }
