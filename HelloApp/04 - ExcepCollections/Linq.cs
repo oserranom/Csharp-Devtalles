e11278a9701e8060c95a1c41b604c39817a8204a
Óscar <oscar.serrano91@gmail.com>
2025-10-19 18:35:45 +0200

    Consultas simples con LINQ

--- HelloApp/04 - ExcepCollections/Linq.cs
+++ HelloApp/04 - ExcepCollections/Linq.cs
@@ -28,10 +28,49 @@ partial class Program
         {
             WriteLine(number);
         }
-        
-        foreach(var number in evenNumbersMethod)
+
+        foreach (var number in evenNumbersMethod)
         {
             WriteLine(number);
         }
+
+
+
+
+
+        //Consultas simples
+        List<MarvelCharacter> characters = new List<MarvelCharacter>
+        {
+        new MarvelCharacter { Name = "Peter Parker", Alias = "Spider-Man", Team = "Avengers" },
+        new MarvelCharacter { Name = "Tony Stark", Alias = "Iron Man", Team = "Avengers" },
+        new MarvelCharacter { Name = "Steve Rogers", Alias = "Captain America", Team = "Avengers" },
+        new MarvelCharacter { Name = "Natasha Romanoff", Alias = "Black Widow", Team = "Avengers" },
+        new MarvelCharacter { Name = "T'Challa", Alias = "Black Panther", Team = "Wakanda" },
+        new MarvelCharacter { Name = "Stephen Strange", Alias = "Doctor Strange", Team = "Defenders" }
+        };
+
+        WriteLine("Personajes que pertenecen al team Avengers");
+        //Query
+        var avengersQuery = from c in characters where c.Team == "Avengers" select $"{c.Alias} ({c.Name})";
+        //Method
+        var avengersMethod = characters.Where(c => c.Team == "Avengers");
+
+        foreach (var character in avengersQuery)
+        {
+            WriteLine(character);
+        }
+
+        foreach (var character in avengersMethod)
+        {
+            WriteLine($"{character.Alias} ({character.Name})"); 
+        }
     }
 }
+
+class MarvelCharacter
+{
+    public string? Name { get; set; }
+    public string? Alias { get; set; }
+    public string? Team { get; set; }
+}
+
