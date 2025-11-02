ef8be85121537febe666c963e0d1eeb150d800dc
Óscar <oscar.serrano91@gmail.com>
2025-11-02 18:32:54 +0100

    config TaskMaster

---
+++ HelloApp/06 - TaskMaster/Queries.cs
@@ -0,0 +1,11 @@
+
+namespace TaskMaster
+{
+  public class Queries(List<Task> _tasks)
+  {
+    private List<Task> Tasks = _tasks;
+    
+    // Desarrollar métodos para gestionar las tareas 
+
+    }
+}
--- HelloApp/obj/Debug/net8.0/HelloApp.AssemblyInfo.cs
+++ HelloApp/obj/Debug/net8.0/HelloApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c3e1fef697e1b2f06ac1436a184e91a16e53aa1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6660a72d64250d88439200660057cb58842463f")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
